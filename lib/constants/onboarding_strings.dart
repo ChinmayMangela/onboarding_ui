3151200f9f5574a6d6f78f7e9d6c56e7fa547e52
apple <mangelachinmay7@gmail.com>
2025-01-19 00:00:14 +0530

    feat: Add onboarding strings and images
    This commit introduces the `OnboardingStrings` class, which defines titles, descriptions, and image paths for onboarding screens. It also adds three onboarding images to the `assets/images/` directory and registers the asset path in `pubspec.yaml`.

---
+++ lib/constants/onboarding_strings.dart
@@ -0,0 +1,15 @@
+
+class OnboardingStrings {
+
+  static const String onboardingTitleOne = 'Department of Computer Engineering';
+  static const String onboardingTitleTwo = 'Learning becomes fun and easy';
+  static const String onboardingTitleThree = 'Flexible Learning, Anytime, Anywhere';
+
+  static const String onboardingDescriptionOne = 'Theem College Of Engineering Village Betegaon, Boisar Chilhar Road, Boisar (E), Palghar';
+  static const String onboardingDescriptionTwo = 'Take your learning journey with you on the go, whenever you want.';
+  static const String onboardingDescriptionThree = 'Whether you\'re at home or on the go, access your lessons and practice materials 24/7';
+
+  static const String onboardingImageOne = 'assets/images/onboarding_image_one.png';
+  static const String onboardingImageTwo = 'assets/images/onboarding_image_two.png';
+  static const String onboardingImageThree = 'assets/images/onboarding_image_three.png';
+}
